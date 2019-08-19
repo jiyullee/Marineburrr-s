c61f7bd9eddf7704daa2b7e789f7af702bec406c
jiyullee <syk_09@naver.com>
2019-08-19 18:33:18 +0900

    Add new game

---
+++ Marine/Assets/ClownFish/Script/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float lifeTime;
+    public int direction;
+    private void OnEnable()
+    {
+        StartCoroutine(DisableSelf());
+    }
+
+    IEnumerator DisableSelf()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
